fa1181d72701e74e1878c63d8f10110ee28ec240
MarcelVHsE <54577510+MarcelVHsE@users.noreply.github.com>
2023-03-10 11:45:39 +0100

    extended config

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import wandb
 import yaml
 from torch.utils.data import DataLoader
 from torchsummary import summary
-from pathlib import Path
+import os
 
 from models.ConvNeXt_implementation import ConvNeXt
 from losses.stixel_loss import StixelLoss
@@ -29,13 +29,13 @@ def main():
                                        transform=transforming,
                                        target_transform=target_transforming)
     train_dataloader = DataLoader(training_data, batch_size=config['batch_size'],
-                                  num_workers=config['train_worker'], pin_memory=True)
+                                  num_workers=config['resources']['train_worker'], pin_memory=True)
 
     validation_data = MultiCutStixelData(validation_data_path, data_dir="data/validation",
                                          transform=transforming,
                                          target_transform=target_transforming)
     val_dataloader = DataLoader(validation_data, batch_size=config['batch_size'],
-                                num_workers=config['val_worker'], pin_memory=True, shuffle=True)
+                                num_workers=config['resources']['val_worker'], pin_memory=True, shuffle=True)
 
     # Explore data
     test_features, test_labels = next(iter(val_dataloader))
@@ -45,8 +45,8 @@ def main():
     # Define Model
     model = ConvNeXt(depths=[3]).to(device)
     # Load Weights
-    if config['load_weights']:
-        weights_file = config['weight_file']
+    if config['weights']['load']:
+        weights_file = config['weight']['file']
         model.load_state_dict(torch.load("saved_models/" + weights_file))
         print(f'Weights loaded from: {weights_file}')
     # Loss function
@@ -77,11 +77,11 @@ def main():
     # Training
     if config['training']:
         if config['logging']:
-            wandb_logger = wandb.init(project="Stixel-Multicut",
+            wandb_logger = wandb.init(project=config['logging']['project'],
                                       config = {
                                           "learning_rate": config['learning_rate'],
-                                          "architecture": "ConvNeXt",
-                                          "dataset": "Stixel Multicut",
+                                          "architecture": config['logging']['architecture'],
+                                          "dataset": config['logging']['dataset'],
                                           "epochs": config['num_epochs'],
                                       }
                                       )
@@ -95,11 +95,13 @@ def main():
             eval_loss = evaluate(val_dataloader, model, loss_fn,
                                  device=device)
             # Save model
-            if config['save_model']:
-                Path('/saved_models').mkdir(parents=True, exist_ok=True)
-                weights_path = f"saved_models/StixelNExT_{wandb_logger.name}_epoch-{epoch}_loss-{eval_loss}.pth"
-                torch.save(model.state_dict(), weights_path)
-                print("Saved PyTorch Model State to " + weights_path)
+            if config['weights']['save']:
+                if os.path.isdir('saved_models'):
+                    weights_path = f"saved_models/StixelNExT_{wandb_logger.name}_epoch-{epoch}_loss-{eval_loss}.pth"
+                    torch.save(model.state_dict(), weights_path)
+                    print("Saved PyTorch Model State to " + weights_path)
+                else:
+                    print("Directory doesn't exist.")
 
 
 if __name__ == '__main__':
