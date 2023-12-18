a75943eefd00c86d48f72a5c3897f5c074adf457
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2023-12-18 18:27:58 +0100

    Add gamma parameter and methods in StixelLoss class
    The StixelLoss class is now updated with a new parameter 'gamma'. Two new methods, 'limit_num_cuts' and 'avoid_dense_predictions' were also included. These changes enhance the loss computation by integrating a loss for maximum cuts and a method for handling dense predictions.

--- losses/stixel_loss.py
+++ losses/stixel_loss.py
@@ -4,20 +4,28 @@ import torch
 
 class StixelLoss(nn.Module):
     # threshold means the threshold when (probab) a border is detected
-    def __init__(self, alpha=1.0, beta=.0, threshold=0.5, offset=10.0):
+    def __init__(self, alpha=1.0, beta=0.0, gamma=0.0, threshold=0.5, offset=10.0):
         """Intersection over Union"""
         super().__init__()
         self.alpha = alpha
         self.beta = beta
+        self.gamma = gamma
         self.threshold = threshold
         self.offset = offset
         self.bce_loss = nn.BCELoss(reduction='mean')
 
     def forward(self, inputs, targets):
         loss_bce = self.bce_loss(inputs, targets.squeeze(0))
-        # additional penalty term
+        loss_maximum_cuts = self.limit_num_cuts(inputs)
+        loss_dense_pred = self.dense_pred(inputs)
+        return self.alpha * loss_bce + self.beta * loss_maximum_cuts +self.gamma * loss_dense_pred
+
+    def limit_num_cuts(self, inputs):
         mask = inputs[0] > self.threshold
         penalties = mask.sum(dim=0) - self.offset
         penalties = penalties.clamp(min=0)  # set negative values to 0
-        loss_penalty = penalties.mean()
-        return self.alpha * loss_bce + self.beta * loss_penalty
+        return penalties.mean()
+
+    def avoid_dense_predictions(self, inputs):
+        # TODO: penalty for dense bottom_cut predictions (inputs[..., 1])
+        return 0
